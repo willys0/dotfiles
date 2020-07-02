2afd344471f3850b32fe170e3b7afdbcd268cbda
Christian Mortensen <cnmrtnsn@gmail.com>
2020-07-02 22:53:35 +0200

    Add build tools and git to be installed

--- .naked-setup.sh
+++ .naked-setup.sh
@@ -3,6 +3,8 @@
 # Installs a bunch of nice packages I like.
 
 apt-get update && (
+    apt-get install build-essential;
+    apt-get install git;
     (apt-get install emacs26 || (add-apt-repository ppa:kelleyk/emacs && apt-get update && apt-get install emacs26));
     apt-get install tmux;
     apt-get install llvm-10 clang-10 libclang-10-dev; # emacs irony server depends on this
