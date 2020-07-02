275934aaeb0979f21bac86c4ba68e168ebafecb8
Christian Mortensen <cnmrtnsn@gmail.com>
2020-07-02 22:43:01 +0200

    Add setup script for completely nude machines

---
+++ .naked-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installs a bunch of nice packages I like.
+
+apt-get update && (
+    (apt-get install emacs26 || (add-apt-repository ppa:kelleyk/emacs && apt-get update && apt-get install emacs26));
+    apt-get install tmux;
+    apt-get install llvm-10 clang-10 libclang-10-dev; # emacs irony server depends on this
+    )
